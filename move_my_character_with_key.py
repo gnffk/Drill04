89a5daaf7edd4a771c9252ec53f1955f6f8ba0f9
minsu <gnffk48@naver.com>
2023-09-26 13:43:53 +0900

    방향 추가 및 위 아래 방향 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,7 +6,7 @@ open_canvas(TUK_WIDTH,TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('walking_woman.png')
 
-global running
+global running,x_dir,y_dir
 
 def handle_events():
     events = get_events()
@@ -15,34 +15,38 @@ def handle_events():
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT and event.key == SDLK_UP:
-
+                x_dir = 1
+                y_dir = 1
             elif event.key == SDLK_RIGHT and event.key == SDLK_DOWN:
-
+                x_dir = 1
+                y_dir = -1
             elif event.key == SDLK_RIGHT:
-
+                x_dir = 1
+                y_dir = 0
             elif event.key == SDLK_LEFT and event.key == SDLK_UP:
-
+                x_dir = -1
+                y_dir = 1
             elif event.key == SDLK_LEFT and event.key == SDLK_DOWN:
-
+                x_dir = -1
+                y_dir = -1
             elif event.key == SDLK_LEFT:
-
+                x_dir = -1
+                y_dir = 0
+            elif event.key == SDLK_UP:
+                x_dir = 0
+                y_dir = 1
+            elif event.key == SDLK_DOWN:
+                x_dir = 0
+                y_dir = -1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT and event.key == SDLK_UP:
-
-            elif event.key == SDLK_RIGHT and event.key == SDLK_DOWN:
-
-            elif event.key == SDLK_RIGHT:
-
-            elif event.key == SDLK_LEFT and event.key == SDLK_UP:
-
-            elif event.key == SDLK_LEFT and event.key == SDLK_DOWN:
-
-            elif event.key == SDLK_LEFT:
-
-            elif event.key == SDLK_ESCAPE:
+            x_dir = 0
+            y_dir = 0
+            if event.key == SDLK_ESCAPE:
                 running = False
+pass
+
 
 
 
