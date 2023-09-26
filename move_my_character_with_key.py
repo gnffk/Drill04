db65103546f3217e5b98cb7199a2d2d5a6d0948c
minsu <gnffk48@naver.com>
2023-09-26 14:19:55 +0900

    움직임 화면 구현
    키가 안먹힘

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,7 +6,7 @@ open_canvas(TUK_WIDTH,TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('walking_woman.png')
 
-global running,x_dir,y_dir
+global running,x_dir,y_dir, walking_motion
 
 def handle_events():
     events = get_events()
@@ -17,35 +17,61 @@ def handle_events():
             if event.key == SDLK_RIGHT and event.key == SDLK_UP:
                 x_dir = 1
                 y_dir = 1
+                walking_motion = 2
             elif event.key == SDLK_RIGHT and event.key == SDLK_DOWN:
                 x_dir = 1
                 y_dir = -1
+                walking_motion = 0
             elif event.key == SDLK_RIGHT:
                 x_dir = 1
                 y_dir = 0
+                walking_motion = 1
             elif event.key == SDLK_LEFT and event.key == SDLK_UP:
                 x_dir = -1
                 y_dir = 1
+                walking_motion = 4
             elif event.key == SDLK_LEFT and event.key == SDLK_DOWN:
                 x_dir = -1
                 y_dir = -1
+                walking_motion = 6
             elif event.key == SDLK_LEFT:
                 x_dir = -1
                 y_dir = 0
+                walking_motion = 5
             elif event.key == SDLK_UP:
                 x_dir = 0
                 y_dir = 1
+                walking_motion = 3
             elif event.key == SDLK_DOWN:
                 x_dir = 0
                 y_dir = -1
+                walking_motion = 7
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
             x_dir = 0
             y_dir = 0
+            walking_motion = 0
             if event.key == SDLK_ESCAPE:
                 running = False
 pass
+
+running = True
+x = TUK_WIDTH // 2
+y = TUK_HEIGHT // 2
+frame = 0
+walking_motion = 0
+dir =0
+while running:
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame*100,walking_motion*187,100, 187,x,y)
+    update_canvas()
+    handle_events()
+    frame = (frame+1)%13
+    x += dir*5
+    y += dir*5
+    delay(0.05)
 
 
 
